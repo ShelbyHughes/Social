cb3973527f1c8d2ee89f01da1387772f274becb6
Shelby Hughes <shelbyhughes2019@gmail.com>
2020-06-10 14:23:27 -0500

    help me

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "social");
-
-if(mysqli_connect_errno()){
-	echo "Failed to connect: " . mysqli_connect_errno();
-}
-
-$query = mysqli_query($con, "INSERT INTO test VALUES(NULL, 'Reece')");
+require 'config/config.php';
 ?>
 <!DOCTYPE html>
 <html>
